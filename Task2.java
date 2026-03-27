e2d2873575f1bdfef3400f412417348e5bde8f8b
Prince Peñaflor <153121021+prinnsipieeee@users.noreply.github.com>
2026-03-27 10:07:29 +0800

    Add Task2.java with main method and output

---
+++ Task2.java
@@ -0,0 +1,19 @@
+public class Task2 {
+    public static void main(String[] args) {
+        char h = 'H';
+        byte three = 3;
+        short one = 1;
+        int zero = 0;
+
+        char w = 'w';
+        char r = 'r';
+        char d = 'd';
+
+        float decimal = 2.0f;
+        boolean t = true;
+
+        String output = h + "" + three + one + one + zero + " " + w + zero + r + one + d + " " + decimal + " " +  t ;
+
+        System.out.println(output);
+    }
+}
