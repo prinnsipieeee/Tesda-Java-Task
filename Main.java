4bf87c6c24da4112e91d3345e517ac75e28eb8c3
Prince Peñaflor <153121021+prinnsipieeee@users.noreply.github.com>
2026-03-28 07:17:06 +0000

    Updated

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Kamusta Mundo!");
+        System.out.println("Kamusta Mundohadkshdsa!");
     }
         
     
